ce63bc28fdb11b2566cc17f2ecf9fec9d4c11b04
lukasz-f <lukaszf6@gmail.com>
2015-07-14 21:30:24 +0200

    clean up

--- src/app.js
+++ src/app.js
@@ -1,32 +1,9 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
-var Vector2 = require('vector2');
 
 var main = new UI.Card({
   title: 'Traffic Light Control',
-  //subtitle: 'Traffic Light Control',
   body: 'Sending...',
   banner: 'images/send4.png'
 });
 
 main.show();
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
