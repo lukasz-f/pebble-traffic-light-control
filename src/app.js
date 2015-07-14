952c422a68aab52ed3368ac17e85b428db2c4718
lukasz-f <lukaszf6@gmail.com>
2015-07-14 21:29:26 +0200

    First version finished

--- src/app.js
+++ src/app.js
@@ -8,30 +8,13 @@ var UI = require('ui');
 var Vector2 = require('vector2');
 
 var main = new UI.Card({
-  banner: 'IMAGE_SEND_IDENTIFIER'
+  title: 'Traffic Light Control',
+  //subtitle: 'Traffic Light Control',
+  body: 'Sending...',
+  banner: 'images/send4.png'
 });
 
 main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
 
 main.on('click', 'select', function(e) {
   var wind = new UI.Window({
@@ -46,12 +29,4 @@ main.on('click', 'select', function(e) {
   });
   wind.add(textfield);
   wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
 });
